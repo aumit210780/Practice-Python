3e7bbeee17ee62cdd065a03ad8dad53a464582f9
Aumit Hasan <76558245+aumit210780@users.noreply.github.com>
2021-03-11 15:27:35 +0600

    Add files via upload

---
+++ If else.py
@@ -0,0 +1,13 @@
+#if-else
+"""
+if condition:
+    statement
+else:
+    statement
+"""
+num = int(input("Enter a number to check whether it is even and odd: "))
+
+if num%2==0:
+    print("Number is Even")
+else:
+    print("Number is odd") 
